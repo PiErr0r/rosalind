e5c1255ecd3f0f776220090edff16405345f08fe
Piero <piero@prehnit.hr>
2020-07-18 23:00:11 +0200

    + some protein mass fun

--- mv_in.sh
+++ mv_in.sh
@@ -1,2 +1,2 @@
-for i in $(ls 00*); do a=$(echo $i | sed -E 's/_.*//'); done;
+for i in $(ls 0**); do a=$(echo $i | sed -E 's/_.*//'); done;
 for i in $(ls *rosa*); do mv $i $a$(echo $i | sed -E 's/rosalind//'); done;
